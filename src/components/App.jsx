0bb3f00bd553dd26bf7cf821f61c25dc1168717d
Matthew Clarke <m.d.clarke.sa@gmail.com>
2025-02-28 11:37:13 +0200

    fix: memoization of objects and functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useMemo, useCallback } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BryntumCalendar } from '@bryntum/calendar-react';
 import Cookies from 'js-cookie';
@@ -17,7 +17,7 @@ function App() {
     const [accessToken, setAccessToken] = useState();
     const [isLoading, setIsLoading] = useState(true);
 
-    const syncData = ({ action, records }) => {
+    const syncData = useCallback(({ action, records }) => {
         if (action === 'add') {
             return;
         }
@@ -33,9 +33,9 @@ function App() {
                 accessToken
             );
         });
-    };
+    }, [accessToken]);
 
-    const addRecord = ({ eventRecord }) => {
+    const addRecord = useCallback(({ eventRecord }) => {
         if (
             eventRecord.id.startsWith('_generated')
         ) {
@@ -62,9 +62,9 @@ function App() {
                 accessToken
             );
         }
-    };
+    }, [accessToken]);
 
-    const calendarConfig = {
+    const calendarConfig = useMemo(() => ({
         defaultMode      : 'month',
         eventEditFeature : {
             items : {
@@ -82,7 +82,7 @@ function App() {
         },
         onDataChange     : syncData,
         onAfterEventSave : addRecord
-    };
+    }), [syncData, addRecord]);
 
     useEffect(() => {
         async function fetchData() {
